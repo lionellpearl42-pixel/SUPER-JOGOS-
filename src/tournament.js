f2dd2ce557b8f68a4c80cf3d0d45911e9285853b
lionellpearl42-pixel <lionellpearl42@gmail.com>
2026-02-13 16:30:16 -0300

    Update tournament.js

--- src/tournament.js
+++ src/tournament.js
@@ -1,16 +1,1 @@
-const { getUser, updateUser } = require("./database");
 
-async function addTournamentPoints(userId, points) {
-  const user = await getUser(userId);
-  user.tournament_points += points;
-  await updateUser(userId, user);
-}
-
-async function leaderboard() {
-  const res = await require("./database").pool.query(
-    "SELECT telegram_id, tournament_points FROM users ORDER BY tournament_points DESC LIMIT 10"
-  );
-  return res.rows;
-}
-
-module.exports = { addTournamentPoints, leaderboard };
