3107a0bda0f63f4adb81a7450831545735040bb3
lionellpearl42-pixel <lionellpearl42@gmail.com>
2026-02-13 16:29:17 -0300

    Update ai.js

--- src/ai.js
+++ src/ai.js
@@ -1,16 +1,1 @@
-const axios = require("axios");
 
-async function narrate(text) {
-  try {
-    const res = await axios.post(
-      "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2",
-      { inputs: text },
-      { headers: { Authorization: `Bearer ${process.env.HF_TOKEN}` } }
-    );
-    return res.data[0].generated_text;
-  } catch {
-    return text;
-  }
-}
-
-module.exports = { narrate };
